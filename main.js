6aa12bafe5960f5910459f44b7c93362752ae69d
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-23 14:24:25 -0500

    finished my calculator. created a function to make code less verbose

--- main.js
+++ main.js
@@ -10,45 +10,63 @@ const acceptInput = () => {
     let inputOne = parseInt(document.getElementById("input-one").value);
 
     let inputTwo = parseInt(document.getElementById("input-two").value);
+
+    return [inputOne, inputTwo];
 }
 
+// addBtn.addEventListener("click", function(){
+//     let inputOne = parseInt(document.getElementById("input-one").value);
+
+//     let inputTwo = parseInt(document.getElementById("input-two").value);
+
+//     console.log(inputOne + inputTwo);      
+// })
+
+// subtractBtn.addEventListener("click", function(){
+//     let inputOne = parseInt(document.getElementById("input-one").value);
+
+//     let inputTwo = parseInt(document.getElementById("input-two").value);
+
+//     console.log(inputOne - inputTwo);      
+// })
+
+// multiplyBtn.addEventListener("click", function(){
+//     let inputOne = parseInt(document.getElementById("input-one").value);
+
+//     let inputTwo = parseInt(document.getElementById("input-two").value);
+
+//     console.log(inputOne * inputTwo);      
+// })
+
+// divideBtn.addEventListener("click", function(){
+//     let inputOne = parseInt(document.getElementById("input-one").value);
+
+//     let inputTwo = parseInt(document.getElementById("input-two").value);
+
+//     console.log(inputOne / inputTwo);      
+// })
+
+
 addBtn.addEventListener("click", function(){
-    let inputOne = parseInt(document.getElementById("input-one").value);
-
-    let inputTwo = parseInt(document.getElementById("input-two").value);
-
-    console.log(inputOne + inputTwo);      
+        acceptInput();
+        console.log(acceptInput()[0] + acceptInput()[1]);         
 })
 
 subtractBtn.addEventListener("click", function(){
-    let inputOne = parseInt(document.getElementById("input-one").value);
-
-    let inputTwo = parseInt(document.getElementById("input-two").value);
-
-    console.log(inputOne - inputTwo);      
+    acceptInput();
+    console.log(acceptInput()[0] - acceptInput()[1]);         
 })
 
 multiplyBtn.addEventListener("click", function(){
-    let inputOne = parseInt(document.getElementById("input-one").value);
-
-    let inputTwo = parseInt(document.getElementById("input-two").value);
-
-    console.log(inputOne * inputTwo);      
+    acceptInput();
+    console.log(acceptInput()[0] * acceptInput()[1]);         
 })
 
 divideBtn.addEventListener("click", function(){
-    let inputOne = parseInt(document.getElementById("input-one").value);
-
-    let inputTwo = parseInt(document.getElementById("input-two").value);
-
-    console.log(inputOne / inputTwo);      
+    acceptInput();
+    console.log(acceptInput()[0] / acceptInput()[1]);         
 })
 
-
-// addBtn.addEventListener("click", function(){
-//         acceptInput();
-//         return inputOne + inputTwo;      
-// })
 
 
 
